257f64e6d651da30d0dda15889d125785681d299
SubirSam <saptarshi.subir@gmail.com>
2022-08-28 15:50:30 +0530

    generics on method second

--- src/com/test/generics/Printer.java
+++ src/com/test/generics/Printer.java
@@ -1,7 +1,17 @@
 package com.test.generics;
 
-public class Printer<T> {
+public class Printer<T,V> {
     T whatToPrint;
+    V whoToPrint;
+
+    public V getWhoToPrint() {
+        return whoToPrint;
+    }
+
+    public void setWhoToPrint(V whoToPrint) {
+        this.whoToPrint = whoToPrint;
+    }
+
     public T getWhatToPrint() {
         return whatToPrint;
     }
