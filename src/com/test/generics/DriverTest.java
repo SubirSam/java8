dec1080b4b228f9cb09274580dfa58629db2ea70
SubirSam <saptarshi.subir@gmail.com>
2022-08-28 15:50:03 +0530

    generics on method second

--- src/com/test/generics/DriverTest.java
+++ src/com/test/generics/DriverTest.java
@@ -9,18 +9,27 @@ public class DriverTest {
         List<Integer> listOfInteger = Arrays.asList(10,2,3,4,5,6,7,89,22);
         System.out.println(refactorTest(listOfInteger));
 
-        Printer<Integer> ptr = new Printer();
+        Printer<Integer,String> ptr = new Printer();
         ptr.setWhatToPrint(11);
+        ptr.setWhoToPrint("sam");
         System.out.println(ptr.getWhatToPrint());
+        System.out.println(ptr.getWhoToPrint());
 
-        Printer<String> ptr1 = new Printer();
+        Printer<String,Integer> ptr1 = new Printer();
         ptr1.setWhatToPrint("India!!!!");
+        ptr1.setWhoToPrint(22);
         System.out.println(ptr1.getWhatToPrint());
+        System.out.println(ptr1.getWhoToPrint());
+
+        System.out.println(test(877879.9898,25));
 
 
+    }
 
-
-
+    public static <T,V> T test(T input, V output){
+        System.out.println(input instanceof Double?"Double!!!!":"String");
+        System.out.println(output instanceof Integer?"Integer!!!!":"String");
+        return input;
     }
     private static List<?> refactorTest(List<?> listOfSomeThing) {
        return listOfSomeThing.stream().filter(s-> s instanceof Integer && (Integer)s % 2==0).collect(Collectors.toList());
